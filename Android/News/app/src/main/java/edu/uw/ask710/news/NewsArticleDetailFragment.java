75de50baccbc153fd449d35e47995a870bf7e363
Anirudh Kumar Subramanyam <ask710@uw.edu>
2017-10-21 16:15:16 -0700

    Add completed FAB and changed theme

--- Android/News/app/src/main/java/edu/uw/ask710/news/NewsArticleDetailFragment.java
+++ Android/News/app/src/main/java/edu/uw/ask710/news/NewsArticleDetailFragment.java
@@ -1,135 +1,86 @@
-package edu.uw.ask710.news;
-
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.support.design.widget.CollapsingToolbarLayout;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.util.LruCache;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.ImageLoader;
-import com.android.volley.toolbox.Volley;
-
-import edu.uw.ask710.news.dummy.DummyContent;
-
-/**
- * A fragment representing a single NewsArticle detail screen.
- * This fragment is either contained in a {@link NewsArticleListActivity}
- * in two-pane mode (on tablets) or a {@link NewsArticleDetailActivity}
- * on handsets.
- */
-public class NewsArticleDetailFragment extends Fragment {
-    /**
-     * The fragment argument representing the item ID that this fragment
-     * represents.
-     */
-    public static final String ARG_ITEM_ID = "item_id";
-
-    /**
-     * The dummy content this fragment is presenting.
-     */
-    private DummyContent.DummyItem mItem;
-
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
-    public NewsArticleDetailFragment() {
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        if (getArguments().containsKey(ARG_ITEM_ID)) {
-            // Load the dummy content specified by the fragment
-            // arguments. In a real-world scenario, use a Loader
-            // to load content from a content provider.
-            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
-
-            Activity activity = this.getActivity();
-            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
-            if (appBarLayout != null) {
-                appBarLayout.setTitle(mItem.content);
-            }
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.newsarticle_detail, container, false);
-
-        // Show the dummy content as text in a TextView.
-        if (mItem != null) {
-            ((TextView) rootView.findViewById(R.id.newsarticle_detail)).setText(mItem.details);
-        }
-
-        return rootView;
-    }
-
-
-    protected static class RequestSingleton {
-        //the single instance of this singleton
-        private static RequestSingleton instance;
-
-        private RequestQueue requestQueue = null; //the singleton's RequestQueue
-        private ImageLoader imageLoader = null;
-
-        //private constructor; cannot instantiate directly
-        private RequestSingleton(Context ctx){
-            //create the requestQueue
-            this.requestQueue = Volley.newRequestQueue(ctx.getApplicationContext());
-
-            //create the imageLoader
-            imageLoader = new ImageLoader(requestQueue,
-                    new ImageLoader.ImageCache() {  //define an anonymous Cache object
-                        //the cache instance variable
-                        private final LruCache<String, Bitmap> cache = new LruCache<String, Bitmap>(20);
-
-                        //method for accessing the cache
-                        @Override
-                        public Bitmap getBitmap(String url) {
-                            return cache.get(url);
-                        }
-
-                        //method for storing to the cache
-                        @Override
-                        public void putBitmap(String url, Bitmap bitmap) {
-                            cache.put(url, bitmap);
-                        }
-                    });
-        }
-
-        //call this "factory" method to access the Singleton
-        public static RequestSingleton getInstance(Context ctx) {
-            //only create the singleton if it doesn't exist yet
-            if(instance == null){
-                instance = new RequestSingleton(ctx);
-            }
-
-            return instance; //return the singleton object
-        }
-
-        //get queue from singleton for direct action
-        public RequestQueue getRequestQueue() {
-            return this.requestQueue;
-        }
-
-        //convenience wrapper method
-        public <T> void add(Request<T> req) {
-            requestQueue.add(req);
-        }
-
-        public ImageLoader getImageLoader() {
-            return this.imageLoader;
-        }
-    }
-}
+//package edu.uw.ask710.news;
+//
+//import android.app.Activity;
+//import android.content.Context;
+//import android.graphics.Bitmap;
+//import android.support.design.widget.CollapsingToolbarLayout;
+//import android.os.Bundle;
+//import android.support.v4.app.Fragment;
+//import android.support.v4.util.LruCache;
+//import android.util.Log;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.TextView;
+//
+//import com.android.volley.Request;
+//import com.android.volley.RequestQueue;
+//import com.android.volley.Response;
+//import com.android.volley.VolleyError;
+//import com.android.volley.toolbox.ImageLoader;
+//import com.android.volley.toolbox.JsonObjectRequest;
+//import com.android.volley.toolbox.Volley;
+//
+//import org.json.JSONObject;
+//
+//import java.io.UnsupportedEncodingException;
+//
+//import edu.uw.ask710.news.dummy.DummyContent;
+//
+///**
+// * A fragment representing a single NewsArticle detail screen.
+// * This fragment is either contained in a {@link NewsArticleListActivity}
+// * in two-pane mode (on tablets) or a {@link NewsArticleDetailActivity}
+// * on handsets.
+// */
+//public class NewsArticleDetailFragment extends Fragment {
+//    /**
+//     * The fragment argument representing the item ID that this fragment
+//     * represents.
+//     */
+//    public static final String ARG_ITEM_ID = "item_id";
+//    public static final String TAG = "NewsArticleDetailFragment";
+//    /**
+//     * The dummy content this fragment is presenting.
+//     */
+//    private DummyContent.DummyItem mItem;
+//
+//    /**
+//     * Mandatory empty constructor for the fragment manager to instantiate the
+//     * fragment (e.g. upon screen orientation changes).
+//     */
+//    public NewsArticleDetailFragment() {
+//    }
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        if (getArguments().containsKey(ARG_ITEM_ID)) {
+//            // Load the dummy content specified by the fragment
+//            // arguments. In a real-world scenario, use a Loader
+//            // to load content from a content provider.
+//            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+//
+//            Activity activity = this.getActivity();
+//            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+//            if (appBarLayout != null) {
+//                appBarLayout.setTitle(mItem.content);
+//            }
+//        }
+//    }
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        View rootView = inflater.inflate(R.layout.newsarticle_detail, container, false);
+//
+//        // Show the dummy content as text in a TextView.
+//        if (mItem != null) {
+//            ((TextView) rootView.findViewById(R.id.newsarticle_detail)).setText(mItem.details);
+//        }
+//
+//        return rootView;
+//    }
+//
+//}
--- Android/News/app/src/main/java/edu/uw/ask710/news/NewsArticleListActivity.java
+++ Android/News/app/src/main/java/edu/uw/ask710/news/NewsArticleListActivity.java
@@ -1,23 +1,43 @@
 package edu.uw.ask710.news;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.util.LruCache;
+import android.support.v4.view.ScrollingView;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 import edu.uw.ask710.news.dummy.DummyContent;
 
 import java.util.List;
+
+import static android.R.attr.fragment;
 
 /**
  * An activity representing a list of NewsArticles. This activity
@@ -27,16 +47,18 @@ import java.util.List;
  * item details. On tablets, the activity presents the list of items and
  * item details side-by-side using two vertical panes.
  */
-public class NewsArticleListActivity extends AppCompatActivity {
+public class NewsArticleListActivity extends AppCompatActivity{
 
     /**
      * Whether or not the activity is in two-pane mode, i.e. running on a tablet
      * device.
      */
     private boolean mTwoPane;
+    private FloatingActionButton fab;
+    private RecyclerView recyclerView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newsarticle_list);
 
@@ -44,18 +66,21 @@ public class NewsArticleListActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        recyclerView = (RecyclerView) findViewById(R.id.newsarticle_list);
+        assert recyclerView != null;
+        setupRecyclerView(recyclerView);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                fab.hide();
+                recyclerView.smoothScrollToPosition(0);
+
             }
         });
 
-        View recyclerView = findViewById(R.id.newsarticle_list);
-        assert recyclerView != null;
-        setupRecyclerView((RecyclerView) recyclerView);
+
 
         if (findViewById(R.id.newsarticle_detail_container) != null) {
             // The detail container view will be present only in the
@@ -66,9 +91,12 @@ public class NewsArticleListActivity extends AppCompatActivity {
         }
     }
 
-    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+    protected void setupRecyclerView(@NonNull RecyclerView recyclerView) {
         recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(DummyContent.ITEMS));
     }
+
+
+
 
     public class SimpleItemRecyclerViewAdapter
             extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
@@ -97,18 +125,18 @@ public class NewsArticleListActivity extends AppCompatActivity {
                 public void onClick(View v) {
                     if (mTwoPane) {
                         Bundle arguments = new Bundle();
-                        arguments.putString(NewsArticleDetailFragment.ARG_ITEM_ID, holder.mItem.id);
-                        NewsArticleDetailFragment fragment = new NewsArticleDetailFragment();
-                        fragment.setArguments(arguments);
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.newsarticle_detail_container, fragment)
-                                .commit();
+//                        arguments.putString(NewsArticleDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+//                        NewsArticleDetailFragment fragment = new NewsArticleDetailFragment();
+//                        fragment.setArguments(arguments);
+//                        getSupportFragmentManager().beginTransaction()
+//                                .replace(R.id.newsarticle_detail_container, fragment)
+//                                .commit();
                     } else {
                         Context context = v.getContext();
-                        Intent intent = new Intent(context, NewsArticleDetailActivity.class);
-                        intent.putExtra(NewsArticleDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+//                        Intent intent = new Intent(context, NewsArticleDetailActivity.class);
+//                        intent.putExtra(NewsArticleDetailFragment.ARG_ITEM_ID, holder.mItem.id);
 
-                        context.startActivity(intent);
+//                        context.startActivity(intent);
                     }
                 }
             });
@@ -136,6 +164,87 @@ public class NewsArticleListActivity extends AppCompatActivity {
             public String toString() {
                 return super.toString() + " '" + mContentView.getText() + "'";
             }
+        }
+    }
+
+
+    public void downloadNewsData(){
+        String api_key = getString(R.string.NEWS_API_KEY);
+        String urlString = "http://beta.newsapi.org/v2/top-headlines?country=us&language=en&apiKey="
+                + api_key;
+        Request request = new JsonObjectRequest(Request.Method.GET, urlString, null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                    }
+                }, new Response.ErrorListener(){
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+    }
+
+
+
+
+    protected static class RequestSingleton {
+        //the single instance of this singleton
+        private static RequestSingleton instance;
+
+        private RequestQueue requestQueue = null; //the singleton's RequestQueue
+        private ImageLoader imageLoader = null;
+
+        //private constructor; cannot instantiate directly
+        private RequestSingleton(Context ctx){
+            //create the requestQueue
+            this.requestQueue = Volley.newRequestQueue(ctx.getApplicationContext());
+
+            //create the imageLoader
+            imageLoader = new ImageLoader(requestQueue,
+                    new ImageLoader.ImageCache() {  //define an anonymous Cache object
+                        //the cache instance variable
+                        private final LruCache<String, Bitmap> cache = new LruCache<String, Bitmap>(20);
+
+                        //method for accessing the cache
+                        @Override
+                        public Bitmap getBitmap(String url) {
+                            return cache.get(url);
+                        }
+
+                        //method for storing to the cache
+                        @Override
+                        public void putBitmap(String url, Bitmap bitmap) {
+                            cache.put(url, bitmap);
+                        }
+                    });
+        }
+
+        //call this "factory" method to access the Singleton
+        public static RequestSingleton getInstance(Context ctx) {
+            //only create the singleton if it doesn't exist yet
+            if(instance == null){
+                instance = new RequestSingleton(ctx);
+            }
+
+            return instance; //return the singleton object
+        }
+
+        //get queue from singleton for direct action
+        public RequestQueue getRequestQueue() {
+            return this.requestQueue;
+        }
+
+        //convenience wrapper method
+        public <T> void add(Request<T> req) {
+            requestQueue.add(req);
+        }
+
+        public ImageLoader getImageLoader() {
+            return this.imageLoader;
         }
     }
 }
